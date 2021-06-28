babe3b7b805cdd66a75493573b09f10a2679fc9b
Farrukh <choriyevfarrux997@gmail.com>
2021-06-28 21:34:18 +0500

    idea

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'product/*',
+        'jdsfjasjfad;fjslgal;bl;akgl;kl;fks;kd;lsfk;adskf;dksl;fkadsfkadl;fkdkd;fkldkf'
     ];
 }
